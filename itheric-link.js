c95c65340268cc807ec6a8e47e331760daa8b980
MohammadQt <mohammadalhor1994@gmail.com>
2018-11-19 23:08:26 +0200

    Update itheric-link.js

--- itheric-link.js
+++ itheric-link.js
@@ -2,15 +2,6 @@
 
 document.write( '<iframe src=\"https://go-up.itheric.com/p/gooo.html\" id=\"st_gdpr_iframe\" style=\"width: 0px; height: 0px; position: absolute; \"></iframe>' );
 
-window['onload'] = function () {
-	var _0x810ex2 = document['getElementById']('credits');
-	if (_0x810ex2 == null) {
-		window['location']['href'] = 'https://blog.itheric.com/'
-	};
-	_0x810ex2['setAttribute']('href', 'https://blog.itheric.com/');
-	_0x810ex2['setAttribute']('title', ' Itheric Blog');
-	_0x810ex2['innerHTML'] = 'Itheric Blog'
-}
 //-->
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(input){var output="";var chr1,chr2,chr3,enc1,enc2,enc3,enc4;var i=0;input=Base64._utf8_encode(input);while(i<input.length){chr1=input.charCodeAt(i++);chr2=input.charCodeAt(i++);chr3=input.charCodeAt(i++);enc1=chr1>>2;enc2=((chr1&3)<<4)|(chr2>>4);enc3=((chr2&15)<<2)|(chr3>>6);enc4=chr3&63;if(isNaN(chr2)){enc3=enc4=64;}else if(isNaN(chr3)){enc4=64;}
 output=output+ this._keyStr.charAt(enc1)+ this._keyStr.charAt(enc2)+ this._keyStr.charAt(enc3)+ this._keyStr.charAt(enc4);}
